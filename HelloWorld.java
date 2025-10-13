70c07ed6bf19e367d2a8394968696af04c49f9df
wuyaxin <2749475736@qq.com>
2025-10-13 10:10:17 +0800

    几

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * HelloWorld 类
+ * 
+ * 这是一个简单的 Java 程序，用于演示基本的输出功能。
+ * 主要用途是验证 Java 开发环境是否配置成功。
+ * 
+ * @author 用户
+ * @version 1.0
+ */
+public class HelloWorld {
+    /**
+     * 主方法：程序的入口点
+     * 
+     * @param args 命令行参数（未使用）
+     */
+    public static void main(String[] args) {
+        // 输出欢迎信息到控制台
+        System.out.println("Hello, Java!");
+    }
+}
