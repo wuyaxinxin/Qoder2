932970f252236f3845cc225702fc486e7f9a9908
wuyaxin <2749475736@qq.com>
2025-12-20 15:51:10 +0800

    武器

--- File3.java
+++ File3.java
@@ -1,39 +1,8 @@
-始终生效
-自由万岁
-hello world
+// 始终生效
+// hello world
 
-import java.util.Set;
-import java.util.HashSet;
-import java.util.Iterator;
-
-public class file3 {
-    public static void demonstrateSetTraversal() {
-        Set<String> set = new HashSet<>();
-        set.add("Apple");
-        set.add("Banana");
-        set.add("Cherry");
-        set.add("Date");
-        set.add("Elderberry");
-        
-        System.out.println("=== 使用 for-each 遍历 ===");
-        for (String item : set) {
-            System.out.println(item);
-        }
-        
-        System.out.println("\n=== 使用 Iterator 遍历 ===");
-        Iterator<String> iterator = set.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
-        
-        System.out.println("\n=== 使用 Stream 遍历 ===");
-        set.stream().forEach(System.out::println);
-        
-        System.out.println("\n=== 使用 forEach 方法遍历 ===");
-        set.forEach(System.out::println);
-    }
-    
+public class File3 {
     public static void main(String[] args) {
-        demonstrateSetTraversal();
+        System.out.println("hello");
     }
 }
