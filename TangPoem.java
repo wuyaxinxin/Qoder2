e7f41d8ea8c1c3ff47df3c2e2f6ecdfafb11ac6e
wuyaxinxin <2749475736@qq.com>
2026-01-12 16:18:29 +0800

    阿

--- TangPoem.java
+++ TangPoem.java
@@ -1,3 +1,4 @@
+// 指定java文件生效
 // 模型决策
 // 输出唐诗《静夜思》的Java程序。
 //日投入vx
@@ -8,7 +9,6 @@ public class TangPoem {
         System.out.println("床前明月光，");
         System.out.println("疑是地上霜。");
         System.out.println("举头望明月，");
-        System.out.println("低头思故。");
-        System.out.println("-- 唐诗结束 --");
+        System.out.println("低头思故乡。");
     }
 }
